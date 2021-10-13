70a4441b7f527abd99ec630d0b8534fcd596da42
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-10-13 17:53:59 +0800

    corrected error in train dataset loader, big bug

--- utils/data_utils_wy.py
+++ utils/data_utils_wy.py
@@ -59,7 +59,8 @@ def get_datasets_raw():
     test_ds = torch.utils.data.ConcatDataset(list(local_test_ds.values()))    
     print("testing dataset is composed of", len(test_ds), "images")
 
-    return local_test_ds, central_train_ds, test_ds
+    return local_train_ds, central_train_ds, test_ds
+    # return local_test_ds, central_train_ds, test_ds
 
 def get_datasets():
     """ input: list of workers, loaded untransformed datasets
